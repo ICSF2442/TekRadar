18d95068b1fbb7c5aafddf91e3819d4a5ec6c7b1
ICSF2442 <icsf2442@gmail.com>
2023-12-12 18:34:49 +0000

    Request feito, backend 100% feito needs testing.

--- api/v1/calculus/getResult.php
+++ api/v1/calculus/getResult.php
@@ -8,7 +8,33 @@ $request = new RequestResponse();
 $json = Utils::getRequestBody();
 if ($json == null) {
     echo "ERRO! JSON INVALIDO!";
-
+///formato json (nome das variaveis)(array tamanho 9):
+//(por ordem)
+//aluguerCusto = x /0
+//infApoio = x /1
+//salasReuniao = x /2
+//parqueEstc = x /3
+//bar = x /4
+//wifi = x /5
+//redeTransportes = x /6
+//armazem = x /7
+//dist = x /8
 } else {
+    $valoresUser = null;
+
+    if($json["valoresUser"]!= null){
+        $valoresUser = $json["valoresUser"];
+
+        $parkIdeal = Utils::calcularParkIdeal($valoresUser);
+
+        echo($request->setResult($parkIdeal->toArray())->response(false));
+
+
+    }else{
+        $request->setError("ERRO!");
+        $request->setIsError(true);
+        echo($request->response(false));
+    }
+
 
 }
