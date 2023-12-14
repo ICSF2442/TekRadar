7c6fa744245e47b26edf4dcf13644e2f82ddfea7
Margarida Fazenda <ispg2021101983@ispgaya.pt>
2023-12-14 19:28:48 +0000

    home page

--- home.php
+++ home.php
@@ -1,1 +1,54 @@
-<3
+<?php
+//Inserir codigo PHP
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+        <link rel="stylesheet" href="./frontend/css/header.css">
+        <link rel="stylesheet" href="./frontend/css/home.css">
+        <meta charset="UTF-8" />
+        <title>TekRadar - Home</title>
+        <link rel="icon" type="image/x-icon" href="./imagens/favicon.ico">
+        <div class="header">
+                <div class="div_logo_titulo">
+                        <a href="home.php"> <img class="logotipo" src="imagens/logo.png" /></a>
+                        <a href="home.php">
+                                <div class="titulo">TekRadar</div>
+                        </a>
+                </div>
+                <div class="menu">
+                        <button class="equipa" onclick="location.href='frontend/equipa/equipa.html'">A EQUIPA</button>
+                        <button class="parques" onclick="location.href='frontend/listaParque/parques.html'">PARQUES</button>
+                        <button class="pesquisa">PESQUISA</button>
+                </div>
+        </div>
+</head>
+
+<body>
+        <div class="div">
+                <div class="div-10">
+                        <div class="div-11">
+                                <div class="column">
+                                        <div class="div-12">
+                                                <div class="div-13">
+                                                        <span style="color: rgba(0, 0, 0, 1)">
+                                                                Encontre o seu parque tecnologico perfeito.
+                                                        </span>
+                                                        <span style="color: rgba(60, 110, 113, 1)"><br>A TekRadar ajuda.</span>
+                                                </div>
+                                                <div class="div-14">
+                                                        Com a TekRadar irá encontrar o seu Parque Tecnológico sem problemas com os seus critérios!
+                                                </div>
+                                        </div>
+                                </div>
+                                <div class="column-2">
+                                        <img class="img-3" src="imagens/home.png" />
+                                </div>
+                        </div>
+                </div>
+        </div>
+</body>
+
+</html>
